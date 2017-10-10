88d1984d6a3e54587c3cf1f2aec8b60808200326
Max Corwin <maxcorwin@yahoo.com>
2017-10-10 01:18:28 -0400

    likes: Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Beginning Deploy"
+
+git pull
+mix deps.get
+mix etco.migrate
+
+(cd assets && npm install)
+(cd assets && ./node_modules/brunch/bin/brunch b -p)
+
+mix release.init
+mix phx.digest
+mix release --env=prod
+
+rm -rf ../release_microblog
+mkdir ../release_microblog
+cp _build/prod/rel/microblog/releases/0.0.1/microblog.tar.gz ../release_microblog
+
+cd ../release_microblog
+tar xzvf microblog.tar.gz
+
+./bin/microblog stop || true
+PORT=8000 ./bin/microblog start
+
